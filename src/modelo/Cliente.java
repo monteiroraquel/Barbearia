b4b2d12f117e99976cdf9ad164422b6f330b5962
monteiroraquel <raquel6monteiro@gmail.com>
2025-10-06 12:32:37 -0300

    criação da classe cliente

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,18 @@
+package modelo;
+
+public class Cliente extends Pessoa{
+
+    public Cliente(int id, String nome, String cpf, String telefone, String email, String endereco) {
+        super(id, nome, cpf, telefone, email, endereco);
+    }
+
+    public String toString() {
+        return  "\nId:" + getId() +
+                "\nNome: " + getNome() +
+                "\nCpf: " + getCpf() +
+                "\nTelefone: " + getTelefone() +
+                "\nEmail: " + getEmail() +
+                "\nEndereco: " + getEndereco();
+    }
+}
+
